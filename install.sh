25db09fd5acd1ce3bb703f481debc5943d306fd1
k <angellmon386@gmail.com>
2016-06-02 17:26:08 +0800

    备份还原为移动方式

--- install.sh
+++ install.sh
@@ -6,20 +6,28 @@ function binaryOperation()
     #$2 src
     #$3 dst
     #$4 list
-    for v in $4;do
-        $1 $2/$v $3/$v
+    cmd=$1
+    src=$2
+    dst=$3
+    shift
+    shift
+    shift
+    for v in "$4";do
+        echo "$cmd $src/$v $dst/$v"
+        $cmd $src/$v $dst/$v
     done
 }
+
 backup_dir=/tmp/base_backup
-backup_list=".vimrc .bashrc"
-backup_list_dir=".vim .myscript"
+backup_list='.vimrc .bashrc'
+backup_list_dir='.vim .myscript'
 root=$("pwd")
 mkdir $backup_dir -pv
 
 echo "backup"
 echo "~ =======> $backup_dir"
-binaryOperation "cp" ~ $backup_dir $backup_list
-binaryOperation "cp -R" ~ $backup_dir $backup_list_dir
+binaryOperation "mv" ~ $backup_dir $backup_list
+binaryOperation "mv" ~ $backup_dir $backup_list_dir
 cp recovery.sh $backup_dir/
 
 echo "install"
