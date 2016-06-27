1a2b80fba9e6ae1054c728377605dd26f6fc309c
k <angellmon386@gmail.com>
2016-06-27 14:03:04 +0000

    php_extension_env: download when is neccessary

--- .myscript/php-extension-env.sh
+++ .myscript/php-extension-env.sh
@@ -8,9 +8,15 @@ env=$root/php-env
 mkdir $src -pv
 mkdir $env -pv
 
-wget $php_src -O $src/php-5.6.22.tar.xz
+srcfile=php-5.6.22.tar.xz
+srcpath=$src/$srcfile
+
+if [ ! -f $srcpath]; then
+wget $php_src -O $srcpath
+fi
+
 cd $src
-tar -vxf php-5.6.22.tar.xz
+tar -vxf $srcfile
 cd php-5.6.22
 ./configure --enable-debug --enable-fpm --with-mysql --prefix="$env"
 make && make install
