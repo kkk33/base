1b528ad2d810dbd3232606bd6e6a85f199d55869
k <angellmon386@gmail.com>
2017-01-11 16:06:28 +0800

    add common script

---
+++ .myscript/module/common.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function C()
+{
+cmd=$1
+
+$cmd
+if ( $cmd != 0 ) ; then
+echo "[error]$cmd"
+exit 
+fi
+}
+
+function package_from_git()
+{
+dirname=$1
+gitpath=$2
+
+rm $dirname -rf
+C "git clone $gitpath ./$dirname"
+
+
+cd $dirname
+version=`git log|head -n 1|awk '{print $2}'`
+name=`basename $PWD`
+packagename="$name($version).tar"
+
+cd -
+C "tar -vcf ./$packagename ./$dirname"
+C "gzip ./$packagename"
+
+rm $dirname -rf
+}
+
